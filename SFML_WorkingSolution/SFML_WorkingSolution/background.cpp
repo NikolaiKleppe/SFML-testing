0e9f04b68f2394021181a5051fc2a808c063595d
Nikolai Kleppe <nikolai__92@hotmail.com>
2015-07-23 19:45:22 +0200

    fiks gitignore

--- SFML_WorkingSolution/SFML_WorkingSolution/background.cpp
+++
@@ -1,9 +0,0 @@
-#include <SFML/Graphics.hpp>
-#include "background.h"
-
-
-void Background::setBackground(std::string name, sf::Vector2f pos) {
-	BGI.loadFromFile(name);
-	BG.setTexture(BGI);
-	BG.setPosition(pos);
-}
